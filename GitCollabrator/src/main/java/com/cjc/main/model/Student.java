86c5cb8b2181b1d0f3ae32d3803b8009e656ec6b
nilulilhare <nilulilhare732@gmail.com>
2024-07-02 15:55:56 +0530

    model

--- GitCollabrator/src/main/java/com/cjc/main/model/Student.java
+++ GitCollabrator/src/main/java/com/cjc/main/model/Student.java
@@ -1,5 +1,19 @@
 package com.cjc.main.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int rno;
+	private String name;
+	private String address;
+	private float mark;
 
 }
