d12a2975d153550a61b46afe37f631ac28daaced
nilulilhare <nilulilhare732@gmail.com>
2024-07-02 15:43:39 +0530

    CrudOperation

---
+++ GitCollabrator/src/main/java/com/cjc/main/GitCollabratorApplication.java
@@ -0,0 +1,14 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCollabratorApplication {
+
+	public static void main(String[] args) {
+		System.out.println("This is collabrator");
+		SpringApplication.run(GitCollabratorApplication.class, args);
+	}
+
+}
