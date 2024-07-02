d4707e5abc77006571d5c286da9b8f6e468bca74
GayatriGadbail <gdgadbail2003@gmail.com>
2024-07-02 16:06:37 +0530

    Update StudentController.java

--- GitCollabrator/src/main/java/com/cjc/main/controller/StudentController.java
+++ GitCollabrator/src/main/java/com/cjc/main/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.cjc.main.controller;
-
+@RestController
 public class StudentController {
-
+  @PostMapping("/SaveData")
+  public String postdetails(@RequestBody Student student)
+  {
+        return studentdata;
 }
